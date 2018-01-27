4947e76cf26f29de9821bc1a693863a653ddd7d7
DEBAPRIYA SEN <sen.debapriyo@gmail.com>
2018-01-27 14:14:47 +0530

    Update basic_unittest.py

--- basic_unittest.py
+++ basic_unittest.py
@@ -2,5 +2,6 @@ import unittest
 import hello_world
 
 test_obj = hello_world.print_app()
-assert(test_obj.print_yes_no() == "yes")# or test_obj.print_yes_no() == "no")
+assert(test_obj.print_yes_no() == "yes" or test_obj.print_yes_no() == "no")
+print test_obj.generate_random_name()
 assert(test_obj.print_args("Hello world"), "Hello world")
