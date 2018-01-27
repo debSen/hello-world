74ff1acb8b240ba7c7727f13290c48faf6ad4c47
DEBAPRIYA SEN <sen.debapriyo@gmail.com>
2018-01-27 13:00:42 +0530

    Update basic_unittest.py

--- basic_unittest.py
+++ basic_unittest.py
@@ -2,6 +2,5 @@ import unittest
 import hello_world
 
 test_obj = hello_world.print_app()
-#print test_obj.print_args("Hello world")
-print test_obj.print_yes_no()
+assert(test_obj.print_yes_no() == "yes" or test_obj.print_yes_no() === "no")
 assert(test_obj.print_args("Hello world"), "Hello world")
