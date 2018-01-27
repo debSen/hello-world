5d6fb612e36223be7a74b177d3f8c4e91f162912
DEBAPRIYA SEN <sen.debapriyo@gmail.com>
2018-01-27 12:53:24 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import requests
 
 class print_app(object):
     def __init__(self):
@@ -6,6 +7,11 @@ class print_app(object):
     
     def print_args(self,args):
         print args
+        
+    def print_yes_no():
+        req = requests.get('https://yesno.wtf/api')
+        req_dict = req.json()
+        print req_dict['answer']
     
 
 Obj = print_app()
