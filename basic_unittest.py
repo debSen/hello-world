c8ef298e83e47dcc6c65697e4694952a26524069
DEBAPRIYA SEN <sen.debapriyo@gmail.com>
2018-01-27 12:43:19 +0530

    Update basic_unittest.py

--- basic_unittest.py
+++ basic_unittest.py
@@ -1,6 +1,6 @@
 import unittest
 import hello_world
 
-test_obj = print_app.print_app()
+test_obj = hello_world.print_app()
 print test_obj.print_args()
 assert(hello_world.print_app(), "Hello world")
